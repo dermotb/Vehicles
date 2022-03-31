70fd77a24dafdb87088d10c582f2b9b93a98e380
Dermot Boyle <dermotb@hotmail.com>
2022-03-31 19:55:27 +0100

    Initial commit

---
+++ Vehicles/Controllers/CarController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vehicles.Models;
+
+namespace Vehicles.Controllers
+{
+    public class CarController : Controller
+    {
+        [HttpGet]
+        public ActionResult Display()
+        {
+            Car car = new Car() {Make="Ford", Model="Capri", EngineType=CarEngineType.Petrol, EngineSize=1600 };
+            return View(car);
+        }
+    }
+}
---
+++ Vehicles/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace Vehicles.Models
+{
+    public enum CarEngineType { Petrol, Diesel, Hybrid, EV};
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public CarEngineType EngineType { get; set; }
+        public int EngineSize { get; set; }
+    }
+}
